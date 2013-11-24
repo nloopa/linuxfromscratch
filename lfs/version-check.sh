90c248c9c3d69d4976deb1482ed06aa494796018
Ryan Wang <nloopa@gmail.com>
2013-11-24 20:44:41 +0800

    lfs/version-check.sh: reorder the check seq

--- lfs/version-check.sh
+++ lfs/version-check.sh
@@ -10,7 +10,6 @@ export LC_ALL=C
 
 bash --version | head -n1 | cut -d" " -f2-4
 echo "/bin/sh -> `readlink -f /bin/sh`"
-echo Perl `perl -V:version`
 echo -n "Coreutils: "; chown --version | head -n1 | cut -d")" -f2
 
 tar --version | head -n1
@@ -18,6 +17,7 @@ bzip2 --version 2>&1 < /dev/null | head -n1 | cut -d" " -f1,6-
 gzip --version | head -n1
 xz --version | head -n1
 echo "Texinfo: `makeinfo --version | head -n1`"
+echo Perl `perl -V:version`
 
 find --version | head -n1
 grep --version | head -n1
