6dbe6a59bca2b112e2c2332b2939fe79544ebbd5
Ryan Wang <nloopa@gmail.com>
2013-11-26 13:35:55 +0800

    lfs/get-packages.sh: download the basic packages

---
+++ lfs/get-packages.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+LFS=/mnt/lfs
+CONFIG=meta
+
+function _alert() {
+	echo
+	echo -e "Alert!!!"
+	echo -e "\tThe wget-list and md5sums may not be uptodate."
+	echo -e "\tPlease download the latest lists manually."
+	echo
+	echo -e "\twget-list:"
+	echo -e "\t    http://www.linuxfromscratch.org/lfs/view/development/wget-list"
+	echo -e "\tmd5sums:"
+	echo -e "\t    http://www.linuxfromscratch.org/lfs/view/development/md5sums"
+	echo -e "\tComplete lists:"
+	echo -e "\t    http://www.linuxfromscratch.org/lfs/view/development/chapter03/packages.html"
+	echo
+}
+
+function _mk_src_dir() {
+	mkdir -v $LFS/sources
+	chmod -v a+wt $LFS/sources
+	wget -i $CONFIG/wget-list -P $LFS/sources
+}
+
+function _chk_md5sums() {
+	cp $CONFIG/md5sums $LFS/sources
+	pushd $LFS/sources
+	md5sum -c md5sums
+	popd
+}
+
+_alert
+_mk_src_dir
+_chk_md5sums
+
